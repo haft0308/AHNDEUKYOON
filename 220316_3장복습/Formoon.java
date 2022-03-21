1d2e4cd3e901dfaa308e65c13e87f54f54a4ef92
AhnDeukYoon <ady9011@naver.com>
2022-03-21 15:40:20 +0900

    charge varialbe

---
+++ 220316_3장복습/Formoon.java
@@ -0,0 +1,65 @@
+
+public class Formoon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//System.out.print("*");
+		//System.out.print("*");
+		//System.out.print("*");
+		//System.out.print("*");
+		//System.out.print("*");
+		
+		//System.out.print("*");
+		//System.out.print("11");
+		//System.out.print("*");
+		//System.out.print("11");
+		//System.out.print("*");
+		//System.out.print("11");
+		
+		//System.out.print("**");
+		//System.out.print("1");
+		//System.out.print("**");
+		//System.out.print("2");
+		//System.out.print("**");
+		//System.out.print("3");
+		//for문으로 바꾼다면?
+		
+		//System.out.println("\n-------\n");
+		
+		int i, j, k = 0;
+		
+		for ( i = 1; i <= 5; i++)
+		{ 
+			System.out.print("*");
+		}
+		
+System.out.print("\n-----------------\n");
+
+		for ( j = 1; j<=3; j++)
+		{
+			System.out.print("*11");
+		}
+
+		//for( int i = 0; i<3; i++)
+		//{
+			//System.out.print("*");
+			//for (int j = 0; j < 2; j++)
+			//{
+					//System.out.print("11");
+			//}
+		//}
+		
+System.out.print("\n-----------------\n");
+		
+		
+		for ( k = 1; k<=3; k++ )
+		{	
+			System.out.print("**");
+			System.out.print(k);
+		}
+		
+		
+	}
+
+}
